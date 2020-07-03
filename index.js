18a146ad33fa32805c95dc0fa4703fa0b280b0a8
Petrov Aleksandr <rpm10b@gmail.com>
2020-07-03 15:31:35 +0300

    сделал слайде

---
+++ index.js
@@ -0,0 +1,28 @@
+const refs = {
+	btn: document.querySelector(".js-btn-event"),
+	slider: document.querySelectorAll(".js-slider"),
+};
+refs.btn.addEventListener("click", nextSlid);
+
+let id = 0;
+function nextSlid(event) {
+	addClassName(id);
+	if (event.target.className === "slider__btn btn--right") {
+		id += 1;
+	} else if (event.target.className === "slider__btn btn--left") {
+		id -= 1;
+	}
+
+	if (id >= refs.slider.length) {
+		id = 0;
+	} else if (id < 0) {
+		id = 4;
+	}
+	const nextSlide = refs.slider[id];
+
+	nextSlide.classList.remove("display-none");
+}
+
+function addClassName(id) {
+	refs.slider[id].classList.add("display-none");
+}
